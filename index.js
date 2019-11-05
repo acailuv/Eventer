bdc70ab6abc37cef08eabeac57a131afb7868f09
noachnt <noach.tjahjadi@gmail.com>
2019-11-05 13:14:40 +0700

    Added Footer + Trending + Testimonial

--- index.js
+++ index.js
@@ -16,7 +16,18 @@ app.get('/register.html', function(req, res) {
     res.sendFile(__dirname + '/html/register.html');
 });
 
+//Footer
+app.get('/aboutus.html', function(req, res) {
+    res.sendFile(__dirname + '/html/aboutus.html');
+});
 
+app.get('/followus.html', function(req, res) {
+    res.sendFile(__dirname + '/html/followus.html');
+});
+
+app.get('/contact.html', function(req, res) {
+    res.sendFile(__dirname + '/html/contact.html');
+});
 
 const server = http.listen(3000, function() {
     console.log('Server starts in *:3000');
--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
-    $('#a').on('click', function() {
+    $('#login').on('click', function() {
         $('#main').load('/login.html');
     });
 });
 
 $(document).ready(function() {
-    $('#b').on('click', function() {
+    $('#register').on('click', function() {
         $('#main').load('/register.html');
+    });
+});
+
+
+// footer
+$(document).ready(function() {
+    $('#aboutus').on('click', function() {
+        $('#main').load('/aboutus.html');
+    });
+});
+
+$(document).ready(function() {
+    $('#followus').on('click', function() {
+        $('#main').load('/followus.html');
+    });
+});
+
+$(document).ready(function() {
+    $('#contact').on('click', function() {
+        $('#main').load('/contact.html');
     });
 });
